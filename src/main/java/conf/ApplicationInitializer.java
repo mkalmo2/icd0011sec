c35bcad82ed459851db432790a160959f26d80e5
Märt Kalmo <mart.kalmo@gmail.com>
2018-11-08 10:45:30 +0200

    done state 2018

--- src/main/java/conf/ApplicationInitializer.java
+++ src/main/java/conf/ApplicationInitializer.java
@@ -12,7 +12,7 @@ public class ApplicationInitializer extends
 
     @Override
     protected Class<?>[] getServletConfigClasses() {
-        return new Class[] { SpringConfig.class };
+        return new Class[] { MvcConfig.class };
     }
 
     @Override
