dab22f200eaef2e09f0e1bf19c8a1c2e4923f3d2
Märt Kalmo <mart.kalmo@gmail.com>
2018-11-09 09:53:39 +0200

    starting state 2018

--- src/main/java/conf/SecurityConfig.java
+++ src/main/java/conf/SecurityConfig.java
@@ -2,20 +2,15 @@ package conf;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.web.authentication.logout.LogoutFilter;
 import security.ApiAuthenticationFilter;
-import security.handlers.*;
 
 import javax.servlet.Filter;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
