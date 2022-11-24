61cc32b72a0081040368cd721b048649af51eec6
Märt Kalmo <mart.kalmo@gmail.com>
2022-11-24 09:43:20 +0200

    solutions removed

--- src/main/java/conf/security/ApiAuthenticationFilter.java
+++ src/main/java/conf/security/ApiAuthenticationFilter.java
@@ -1,20 +1,16 @@
 package conf.security;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import conf.security.handlers.ApiAuthFailureHandler;
 import conf.security.handlers.ApiAuthSuccessHandler;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 
 import java.io.IOException;
-import java.util.stream.Collectors;
 
 public class ApiAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
 
@@ -30,16 +26,15 @@ public class ApiAuthenticationFilter extends AbstractAuthenticationProcessingFil
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request,
-                                                HttpServletResponse response) throws IOException {
+                                                HttpServletResponse response) {
 
-        String json = request.getReader().lines().collect(Collectors.joining("\n"));
+        LoginCredentials loginCredentials = new LoginCredentials();
 
-        LoginCredentials loginCredentials;
-        try {
-            loginCredentials = new ObjectMapper().readValue(json, LoginCredentials.class);
-        } catch (JsonProcessingException e) {
-            throw new BadCredentialsException("", e);
-        }
+        // Read info from HttpServletRequest.
+
+        // Use ObjectMapper to convert Json to LoginCredentials object.
+
+        // Info from LoginCredentials is used below.
 
         UsernamePasswordAuthenticationToken token =
                 new UsernamePasswordAuthenticationToken(
--- src/main/java/conf/security/SecurityConfig.java
+++ src/main/java/conf/security/SecurityConfig.java
@@ -1,8 +1,5 @@
 package conf.security;
 
-import conf.security.handlers.ApiAccessDeniedHandler;
-import conf.security.handlers.ApiEntryPoint;
-import conf.security.handlers.ApiLogoutSuccessHandler;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.PropertySource;
@@ -12,14 +9,9 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 @EnableMethodSecurity
@@ -33,52 +25,11 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(
             HttpSecurity http) throws Exception {
 
-//        http.sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-//        http.formLogin();
-
-        http.csrf().disable();
-
-        http.authorizeHttpRequests()
-                .requestMatchers("/api/home").permitAll()
-                .requestMatchers("/api/admin/**").hasRole("ADMIN")
-                .requestMatchers("/api/**").authenticated()
-                .requestMatchers("/**").permitAll();
-
-        http.exceptionHandling()
-                .authenticationEntryPoint(new ApiEntryPoint())
-                .accessDeniedHandler(new ApiAccessDeniedHandler());
-
-        http.logout()
-                .logoutSuccessHandler(new ApiLogoutSuccessHandler())
-                .logoutUrl("/api/logout");
+        // configure filters
 
         http.apply(new FilterConfigurer());
 
         return http.build();
-    }
-
-    @Bean
-    public UserDetailsService userDetailService() {
-        UserDetails user = User.builder()
-                .username("user")
-                .password("$2a$10$e2v79LxWXANrsBB4bkbqaePjGCwruo8sUxX4m0hoYUN3dZ6pEibDG")
-                .roles("USER")
-                .build();
-
-        UserDetails admin = User.builder()
-                .username("admin")
-                .password("$2a$10$e2v79LxWXANrsBB4bkbqaePjGCwruo8sUxX4m0hoYUN3dZ6pEibDG")
-                .roles("USER", "ADMIN")
-                .build();
-
-        return new InMemoryUserDetailsManager(user, admin);
-    }
-
-    @Bean
-    public static PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
     }
 
     public class FilterConfigurer extends AbstractHttpConfigurer<FilterConfigurer, HttpSecurity> {
@@ -86,25 +37,14 @@ public class SecurityConfig {
         public void configure(HttpSecurity http) {
             AuthenticationManager manager = http.getSharedObject(AuthenticationManager.class);
 
-            var loginFilter = new ApiAuthenticationFilter(
-                manager, "/api/login");
+            // add new filters
 
-            http.addFilterBefore(loginFilter,
-                    UsernamePasswordAuthenticationFilter.class);
-
-//            var authorizationFilter = new ApiAuthorizationFilter();
-//
-//            http.addFilterBefore(authorizationFilter, AuthorizationFilter.class);
-
-//            var loginFilter = new JwtAuthenticationFilter(
-//                manager, "/api/login", jwtKey);
-//
-//            http.addFilterBefore(loginFilter, UsernamePasswordAuthenticationFilter.class);
-//
-//            var authorizationFilter = new JwtAuthorizationFilter(jwtKey);
-//
-//            http.addFilterBefore(authorizationFilter, AuthorizationFilter.class);
         }
+    }
+
+    @Bean
+    public static PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Bean
