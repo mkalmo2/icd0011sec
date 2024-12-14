326a396464336d56509788c88edda580015e466d
Märt Kalmo <mart.kalmo@gmail.com>
2024-12-14 17:18:19 +0200

    ApiAuthenticationFilter fix

--- src/main/java/conf/security/ApiAuthenticationFilter.java
+++ src/main/java/conf/security/ApiAuthenticationFilter.java
@@ -1,19 +1,14 @@
 package conf.security;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import conf.security.handlers.ApiAuthFailureHandler;
 import conf.security.handlers.ApiAuthSuccessHandler;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.AbstractAuthenticationProcessingFilter;
-
-import java.io.IOException;
-import java.util.stream.Collectors;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
 
 public class ApiAuthenticationFilter extends AbstractAuthenticationProcessingFilter {
 
@@ -23,20 +18,21 @@ public class ApiAuthenticationFilter extends AbstractAuthenticationProcessingFil
         setAuthenticationManager(authenticationManager);
         setAuthenticationSuccessHandler(new ApiAuthSuccessHandler());
         setAuthenticationFailureHandler(new ApiAuthFailureHandler());
+
+        setSecurityContextRepository(new HttpSessionSecurityContextRepository());
     }
 
     @Override
     public Authentication attemptAuthentication(HttpServletRequest request,
-                                                HttpServletResponse response) throws IOException {
+                                                HttpServletResponse response) {
 
-        String content = request.getReader().lines().collect(Collectors.joining(""));
+        LoginCredentials loginCredentials = new LoginCredentials();
 
-        LoginCredentials loginCredentials;
-        try {
-            loginCredentials = new ObjectMapper().readValue(content, LoginCredentials.class);
-        } catch (JsonProcessingException e) {
-            throw new BadCredentialsException("", e);
-        }
+        // Read info from HttpServletRequest.
+
+        // Use ObjectMapper to convert Json to LoginCredentials object.
+
+        // Info from LoginCredentials is used below.
 
         UsernamePasswordAuthenticationToken token =
                 new UsernamePasswordAuthenticationToken(
